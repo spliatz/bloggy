e03d51e9524ab41c8b3071036391f98a8aaae34d
spliatz <spliatzoff@gmail.com>
2022-12-25 14:54:34 +0400

    feat(main): enable cors

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,26 +1,27 @@
 package main
 
 import (
-    "os"
-    "strconv"
+	"github.com/gin-contrib/cors"
+	"os"
+	"strconv"
 
-    "github.com/gin-gonic/gin"
-    "github.com/sirupsen/logrus"
+	"github.com/gin-gonic/gin"
+	"github.com/sirupsen/logrus"
 
-    _ "github.com/jackc/pgx/v5/pgtype"
+	_ "github.com/jackc/pgx/v5/pgtype"
 
-    _ "github.com/Intellect-Bloggy/bloggy-backend/docs"
-    "github.com/Intellect-Bloggy/bloggy-backend/internal/adapters/db/postgres"
-    "github.com/Intellect-Bloggy/bloggy-backend/internal/controller/http"
-    "github.com/Intellect-Bloggy/bloggy-backend/internal/controller/http/middleware"
-    "github.com/Intellect-Bloggy/bloggy-backend/internal/domain/service"
-    auth_usecase "github.com/Intellect-Bloggy/bloggy-backend/internal/domain/usecase/auth"
-    "github.com/Intellect-Bloggy/bloggy-backend/internal/domain/usecase/post"
-    user_usecase "github.com/Intellect-Bloggy/bloggy-backend/internal/domain/usecase/user"
-    "github.com/Intellect-Bloggy/bloggy-backend/internal/server"
-    "github.com/Intellect-Bloggy/bloggy-backend/pkg/auth"
-    pq_client "github.com/Intellect-Bloggy/bloggy-backend/pkg/client/postgres"
-    "github.com/Intellect-Bloggy/bloggy-backend/pkg/hash"
+	_ "github.com/Intellect-Bloggy/bloggy-backend/docs"
+	"github.com/Intellect-Bloggy/bloggy-backend/internal/adapters/db/postgres"
+	"github.com/Intellect-Bloggy/bloggy-backend/internal/controller/http"
+	"github.com/Intellect-Bloggy/bloggy-backend/internal/controller/http/middleware"
+	"github.com/Intellect-Bloggy/bloggy-backend/internal/domain/service"
+	auth_usecase "github.com/Intellect-Bloggy/bloggy-backend/internal/domain/usecase/auth"
+	"github.com/Intellect-Bloggy/bloggy-backend/internal/domain/usecase/post"
+	user_usecase "github.com/Intellect-Bloggy/bloggy-backend/internal/domain/usecase/user"
+	"github.com/Intellect-Bloggy/bloggy-backend/internal/server"
+	"github.com/Intellect-Bloggy/bloggy-backend/pkg/auth"
+	pq_client "github.com/Intellect-Bloggy/bloggy-backend/pkg/client/postgres"
+	"github.com/Intellect-Bloggy/bloggy-backend/pkg/hash"
 )
 
 // @title Bloggy-backend
@@ -35,70 +36,73 @@ import (
 // @name Authorization
 func main() {
 
-    dbPort, err := strconv.Atoi(os.Getenv("POSTGRES_LOCAL_PORT"))
-    if dbPort > 1<<16 || dbPort < 1 || err != nil {
-        logrus.Fatal("Подключение к базе данных не удалось: некорректный порт")
-    }
+	dbPort, err := strconv.Atoi(os.Getenv("POSTGRES_LOCAL_PORT"))
+	if dbPort > 1<<16 || dbPort < 1 || err != nil {
+		logrus.Fatal("Подключение к базе данных не удалось: некорректный порт")
+	}
 
-    db, err := pq_client.NewPostgresDB(pq_client.PostgresConfig{
-        Host:     os.Getenv("POSTGRES_HOST"),
-        Port:     uint16(dbPort),
-        Username: os.Getenv("POSTGRES_USER"),
-        DBName:   os.Getenv("POSTGRES_DB"),
-        Password: os.Getenv("POSTGRES_PASSWORD"),
-        SSLMode:  os.Getenv("POSTGRES_SSLMode"),
-    })
-    if err != nil {
-        logrus.Fatal("Подключение к базе данных не удалось: ", err)
-    }
+	db, err := pq_client.NewPostgresDB(pq_client.PostgresConfig{
+		Host:     os.Getenv("POSTGRES_HOST"),
+		Port:     uint16(dbPort),
+		Username: os.Getenv("POSTGRES_USER"),
+		DBName:   os.Getenv("POSTGRES_DB"),
+		Password: os.Getenv("POSTGRES_PASSWORD"),
+		SSLMode:  os.Getenv("POSTGRES_SSLMode"),
+	})
+	if err != nil {
+		logrus.Fatal("Подключение к базе данных не удалось: ", err)
+	}
 
-    logrus.Println("База данных успешно подключена")
+	logrus.Println("База данных успешно подключена")
 
-    // TODO: Переделать на ENV
-    // FIXME: Серьезно
-    salt := "random string test"
-    signKey := "random string text"
+	// TODO: Переделать на ENV
+	// FIXME: Серьезно
+	salt := "random string test"
+	signKey := "random string text"
 
-    router := gin.New()
+	router := gin.New()
 
-    // utils
-    hasher := hash.NewSHA1Hasher(salt)
-    tManager, err := auth.NewManager(signKey)
-    if err != nil {
-        logrus.Fatal("Не удалось создать менеджер токенов ", err)
-    }
+	// cors: allow all origins
+	router.Use(cors.Default())
 
-    // middlewares
-    authMiddleware := middleware.NewAuthMiddleware(tManager)
+	// utils
+	hasher := hash.NewSHA1Hasher(salt)
+	tManager, err := auth.NewManager(signKey)
+	if err != nil {
+		logrus.Fatal("Не удалось создать менеджер токенов ", err)
+	}
 
-    // storages
-    userStorage := postgres.NewUserStorage(db)
-    authStorage := postgres.NewAuthStorage(db)
-    postStorage := postgres.NewPostStorage(db)
+	// middlewares
+	authMiddleware := middleware.NewAuthMiddleware(tManager)
 
-    // services
-    authService := service.NewAuthService(authStorage, tManager)
-    userService := service.NewUserService(userStorage, hasher)
-    postService := service.NewPostService(postStorage)
+	// storages
+	userStorage := postgres.NewUserStorage(db)
+	authStorage := postgres.NewAuthStorage(db)
+	postStorage := postgres.NewPostStorage(db)
 
-    // usecases
-    authUsecase := auth_usecase.NewAuthUsecase(authService, userService)
-    userUsecase := user_usecase.NewUserUsecase(userService, postService)
-    postUsecase := post.NewPostUsecase(postService)
+	// services
+	authService := service.NewAuthService(authStorage, tManager)
+	userService := service.NewUserService(userStorage, hasher)
+	postService := service.NewPostService(postStorage)
 
-    // register handlers
-    http.NewAuthHandler(authUsecase).Register(router)
-    http.NewUserHandler(authMiddleware, userUsecase).Register(router)
-    http.NewPostHandler(authMiddleware, postUsecase).Register(router)
-    http.NewDocsHandler().Register(router)
+	// usecases
+	authUsecase := auth_usecase.NewAuthUsecase(authService, userService)
+	userUsecase := user_usecase.NewUserUsecase(userService, postService)
+	postUsecase := post.NewPostUsecase(postService)
 
-    srv := server.NewServer()
-    srvPort, err := strconv.Atoi(os.Getenv("PORT"))
-    if srvPort > 1<<16 || srvPort < 1 || err != nil {
-        logrus.Fatal("Запуск сервера не удался: некорректный порт")
-    }
+	// register handlers
+	http.NewAuthHandler(authUsecase).Register(router)
+	http.NewUserHandler(authMiddleware, userUsecase).Register(router)
+	http.NewPostHandler(authMiddleware, postUsecase).Register(router)
+	http.NewDocsHandler().Register(router)
 
-    if err := srv.Run(uint16(srvPort), router); err != nil {
-        logrus.Fatal("Ошибка запуска сервера: ", err)
-    }
+	srv := server.NewServer()
+	srvPort, err := strconv.Atoi(os.Getenv("PORT"))
+	if srvPort > 1<<16 || srvPort < 1 || err != nil {
+		logrus.Fatal("Запуск сервера не удался: некорректный порт")
+	}
+
+	if err := srv.Run(uint16(srvPort), router); err != nil {
+		logrus.Fatal("Ошибка запуска сервера: ", err)
+	}
 }
