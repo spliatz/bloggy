86d798a87d36c3935db8f57f0e0854d2c32f6edb
spliatz <spliatzoff@gmail.com>
2022-12-25 15:37:53 +0400

    feat(user): /user/my endpoint

--- internal/controller/http/user.go
+++ internal/controller/http/user.go
@@ -1,51 +1,82 @@
 package http
 
 import (
-    "context"
-    "net/http"
+	"context"
+	"net/http"
 
-    "github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin"
 
-    "github.com/Intellect-Bloggy/bloggy-backend/internal/controller/http/response"
-    "github.com/Intellect-Bloggy/bloggy-backend/internal/domain/entity"
-    user_dto "github.com/Intellect-Bloggy/bloggy-backend/internal/domain/usecase/user/dto"
-    "github.com/Intellect-Bloggy/bloggy-backend/pkg/errors"
+	"github.com/Intellect-Bloggy/bloggy-backend/internal/controller/http/response"
+	"github.com/Intellect-Bloggy/bloggy-backend/internal/domain/entity"
+	user_dto "github.com/Intellect-Bloggy/bloggy-backend/internal/domain/usecase/user/dto"
+	"github.com/Intellect-Bloggy/bloggy-backend/pkg/errors"
 )
 
 type authMiddleware interface {
-    UserIdentity(c *gin.Context)
+	UserIdentity(c *gin.Context)
 }
 
 type userUsecase interface {
-    GetByUsername(ctx context.Context, username string) (entity.UserResponse, error)
-    EditById(ctx context.Context, id int, dto user_dto.EditUserDTO) (entity.UserResponse, error)
-    GetAllByUsername(ctx context.Context, username string) (posts []entity.Post, err error)
+	GetById(ctx context.Context, id int) (entity.UserResponse, error)
+	GetByUsername(ctx context.Context, username string) (entity.UserResponse, error)
+	EditById(ctx context.Context, id int, dto user_dto.EditUserDTO) (entity.UserResponse, error)
+	GetAllByUsername(ctx context.Context, username string) (posts []entity.Post, err error)
 }
 
 type userHandler struct {
-    authMiddleware
-    userUsecase
+	authMiddleware
+	userUsecase
 }
 
 func NewUserHandler(authMiddleware authMiddleware, userUsecase userUsecase) *userHandler {
-    return &userHandler{authMiddleware: authMiddleware, userUsecase: userUsecase}
+	return &userHandler{authMiddleware: authMiddleware, userUsecase: userUsecase}
 }
 
 func (h *userHandler) Register(router *gin.Engine) {
-    user := router.Group("/user")
-    {
-        protected := user.Group("", h.authMiddleware.UserIdentity)
-        {
-            protected.PATCH("", h.editById)
-        }
-        user.GET("/:username", h.getByUsername)
-        user.GET("/:username/posts", h.getAllByUsername)
-    }
+	user := router.Group("/user")
+	{
+		protected := user.Group("", h.authMiddleware.UserIdentity)
+		{
+			protected.GET("/my", h.getMy)
+			protected.PATCH("", h.editById)
+		}
+		user.GET("/:username", h.getByUsername)
+		user.GET("/:username/posts", h.getAllByUsername)
+	}
 }
 
 // @Summary GetUserByUsername
 // @Tags user
-// @Description login
+// @Description get my user info
+// @Security ApiKeyAuth
+// @ID get-my
+// @Accept json
+// @Produce json
+// @Success 200 {object} entity.UserResponseSwagger
+// @Failure 400,404 {object} response.ErrorResponse
+// @Failure 500 {object} response.ErrorResponse
+// @Failure default {object} response.ErrorResponse
+// @Router /user/my [get]
+func (h *userHandler) getMy(c *gin.Context) {
+	userIdI, exist := c.Get("user_id")
+	if !exist {
+		response.ResponseWithError(c, errors.ErrIdNotFound)
+		return
+	}
+
+	userId, _ := userIdI.(int)
+	user, err := h.userUsecase.GetById(c.Request.Context(), userId)
+	if err != nil {
+		response.ResponseWithError(c, errors.EtoHe(err))
+		return
+	}
+
+	c.JSON(http.StatusOK, user)
+}
+
+// @Summary GetUserByUsername
+// @Tags user
+// @Description get user by username
 // @ID get-user-by-username
 // @Accept json
 // @Produce json
@@ -56,14 +87,14 @@ func (h *userHandler) Register(router *gin.Engine) {
 // @Failure default {object} response.ErrorResponse
 // @Router /user/{username} [get]
 func (h *userHandler) getByUsername(c *gin.Context) {
-    username := c.Param("username")
-    user, err := h.userUsecase.GetByUsername(c.Request.Context(), username)
-    if err != nil {
-        response.ResponseWithError(c, errors.EtoHe(err))
-        return
-    }
+	username := c.Param("username")
+	user, err := h.userUsecase.GetByUsername(c.Request.Context(), username)
+	if err != nil {
+		response.ResponseWithError(c, errors.EtoHe(err))
+		return
+	}
 
-    c.JSON(http.StatusOK, user)
+	c.JSON(http.StatusOK, user)
 }
 
 // @Summary EditUserByUsername
@@ -80,27 +111,27 @@ func (h *userHandler) getByUsername(c *gin.Context) {
 // @Failure default {object} response.ErrorResponse
 // @Router /user [patch]
 func (h *userHandler) editById(c *gin.Context) {
-    userIdI, exist := c.Get("user_id")
-    if !exist {
-        response.ResponseWithError(c, errors.ErrIdNotFound)
-        return
-    }
+	userIdI, exist := c.Get("user_id")
+	if !exist {
+		response.ResponseWithError(c, errors.ErrIdNotFound)
+		return
+	}
 
-    userId, _ := userIdI.(int)
+	userId, _ := userIdI.(int)
 
-    i := user_dto.EditUserDTO{}
-    if err := c.BindJSON(&i); err != nil {
-        response.ResponseWithError(c, errors.NewHTTPError(http.StatusBadRequest, err))
-        return
-    }
+	i := user_dto.EditUserDTO{}
+	if err := c.BindJSON(&i); err != nil {
+		response.ResponseWithError(c, errors.NewHTTPError(http.StatusBadRequest, err))
+		return
+	}
 
-    user, err := h.userUsecase.EditById(c.Request.Context(), userId, i)
-    if err != nil {
-        response.ResponseWithError(c, errors.EtoHe(err))
-        return
-    }
+	user, err := h.userUsecase.EditById(c.Request.Context(), userId, i)
+	if err != nil {
+		response.ResponseWithError(c, errors.EtoHe(err))
+		return
+	}
 
-    c.JSON(http.StatusOK, user)
+	c.JSON(http.StatusOK, user)
 }
 
 // @Summary GetAllByUsername
@@ -116,12 +147,12 @@ func (h *userHandler) editById(c *gin.Context) {
 // @Failure default {object} response.ErrorResponse
 // @Router /user/{username}/posts [get]
 func (h *userHandler) getAllByUsername(c *gin.Context) {
-    username := c.Param("username")
-    posts, err := h.userUsecase.GetAllByUsername(c.Request.Context(), username)
-    if err != nil {
-        response.ResponseWithError(c, errors.EtoHe(err))
-        return
-    }
+	username := c.Param("username")
+	posts, err := h.userUsecase.GetAllByUsername(c.Request.Context(), username)
+	if err != nil {
+		response.ResponseWithError(c, errors.EtoHe(err))
+		return
+	}
 
-    c.JSON(http.StatusOK, posts)
+	c.JSON(http.StatusOK, posts)
 }
--- internal/domain/usecase/user/user.go
+++ internal/domain/usecase/user/user.go
@@ -1,38 +1,50 @@
 package user
 
 import (
-    "context"
-
-    "github.com/Intellect-Bloggy/bloggy-backend/internal/domain/entity"
-    user_dto "github.com/Intellect-Bloggy/bloggy-backend/internal/domain/usecase/user/dto"
+	"context"
+	"github.com/Intellect-Bloggy/bloggy-backend/internal/domain/entity"
+	user_dto "github.com/Intellect-Bloggy/bloggy-backend/internal/domain/usecase/user/dto"
 )
 
 type service interface {
-    GetByUsername(ctx context.Context, username string) (entity.UserResponse, error)
-    EditById(ctx context.Context, id int, i user_dto.EditUserDTO) (entity.UserResponse, error)
+	GetUserByID(ctx context.Context, id int) (entity.User, error)
+	GetByUsername(ctx context.Context, username string) (entity.UserResponse, error)
+	EditById(ctx context.Context, id int, i user_dto.EditUserDTO) (entity.UserResponse, error)
 }
 
 type postService interface {
-    GetAllByUsername(ctx context.Context, username string) (posts []entity.Post, err error)
+	GetAllByUsername(ctx context.Context, username string) (posts []entity.Post, err error)
 }
 
 type userUsecase struct {
-    service
-    postService
+	service
+	postService
 }
 
 func NewUserUsecase(service service, postService postService) *userUsecase {
-    return &userUsecase{service: service, postService: postService}
+	return &userUsecase{service: service, postService: postService}
+}
+
+func (u *userUsecase) GetById(ctx context.Context, id int) (entity.UserResponse, error) {
+	user, err := u.service.GetUserByID(ctx, id)
+	return entity.UserResponse{
+		Username:  user.Username,
+		Name:      user.Name,
+		Birthday:  user.Birthday,
+		Email:     user.Email,
+		Phone:     user.Phone,
+		CreatedAt: user.CreatedAt,
+	}, err
 }
 
 func (u *userUsecase) GetByUsername(ctx context.Context, username string) (entity.UserResponse, error) {
-    return u.service.GetByUsername(ctx, username)
+	return u.service.GetByUsername(ctx, username)
 }
 
 func (u *userUsecase) GetAllByUsername(ctx context.Context, username string) (posts []entity.Post, err error) {
-    return u.postService.GetAllByUsername(ctx, username)
+	return u.postService.GetAllByUsername(ctx, username)
 }
 
 func (u *userUsecase) EditById(ctx context.Context, id int, dto user_dto.EditUserDTO) (entity.UserResponse, error) {
-    return u.service.EditById(ctx, id, dto)
+	return u.service.EditById(ctx, id, dto)
 }
